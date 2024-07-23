fc2eb8f6532947739f80be45522d55742085f1dc
William Rosa <williamrosaherrera@gmail.com>
2024-07-23 04:07:06 -0600

    hacer el menu responsivo

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BiMenu, BiUser, BiLogOut } from 'react-icons/bi';
+import { BiUser, BiLogOut } from 'react-icons/bi';
 import DarkModeSwitch from './DarkModeSwitch';
 import './Header.css';
 
-const Header = ({ toggleDarkMode, darkMode }) => {
-
+const Header = ({ toggleDarkMode, darkMode, menuCollapsed }) => {
   return (
-    <header className="header-container">
-      <button className="oculto">        
-      </button>
-      <div className="header-right">      
+    <header className={`header-container ${menuCollapsed ? 'collapsed' : ''}`}>
+      <button className="oculto"></button>
+      <div className="header-right">
         <BiUser className="header-icon" />
         <BiLogOut className="header-icon" />
         <DarkModeSwitch darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
--- src/components/Layout/VerticalLayout.jsx
+++ src/components/Layout/VerticalLayout.jsx
@@ -9,79 +9,91 @@ import logoImage from "../../assets/images/logo-dark.png";
 
 const VerticalLayout = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const [menuCollapsed, setMenuCollapsed] = useState(false); // Estado para el colapso del menú
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   };
 
+  const handleMenuToggle = () => {
+    setMenuCollapsed(!menuCollapsed);
+  };
+
   return (
-    <div className={`d-flex vertical-layout ${darkMode ? 'dark-mode' : ''}`}>
-      <Header toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
-      <nav className="vertical-nav fondo text-white">
-        <div className="p-3">
-          <img src={logoImage} alt="Logo" className="logo-img" />
-        </div>
-        <ul className="nav flex-column">
-          <li className="nav-item">
-            <NavLink to="/home" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
-              <BiHome className="nav-icon" />
-              Inicio
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/GestionUsuarios" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
-              <BiUser className="nav-icon" />
-              Usuarios
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/GestionEmpleados" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
-              <BiGroup className="nav-icon" />
-              Empleados
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/GestionClientes" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
-              <BiGroup className="nav-icon" />
-              Clientes
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/GestionPaquetes" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
-              <BiPackage className="nav-icon" />
-              Paquetes
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/rutas" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
-              <BiDirections className="nav-icon" />
-              Rutas
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/vehiculos" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
-              <BiCar className="nav-icon" />
-              Vehículos
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/historial" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
-              <BiShoppingBag className="nav-icon" />
-              Historial
-            </NavLink>
-          </li>
-          <li className="nav-item mt-auto">
-            <NavLink to="/logout" className="nav-link">
-              <BiLogOut className="nav-icon" />
-              Logout
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      <div className="main-content p-4">
+    <div className={`vertical-layout ${darkMode ? 'dark-mode' : ''}`}>
+      <Header toggleDarkMode={toggleDarkMode} darkMode={darkMode} menuCollapsed={menuCollapsed} />
+      <div className="menu-container">
+        <nav className={`vertical-nav fondo text-white ${menuCollapsed ? 'menu-collapsed' : 'menu-expanded'}`}>
+          <div className="p-3">
+            <img
+              src={logoImage}
+              alt="Logo"
+              className={`logo-img ${menuCollapsed ? 'logo-collapsed' : 'logo-expanded'}`}
+              onClick={handleMenuToggle} // Asigna la función de alternar menú
+            />
+          </div>
+          <ul className={`nav flex-column ${menuCollapsed ? 'icons-only' : ''}`}>
+            <li className="nav-item">
+              <NavLink to="/home" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+                <BiHome className="nav-icon" />
+                {!menuCollapsed && 'Inicio'}
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/GestionUsuarios" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+                <BiUser className="nav-icon" />
+                {!menuCollapsed && 'Usuarios'}
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/GestionEmpleados" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+                <BiGroup className="nav-icon" />
+                {!menuCollapsed && 'Empleados'}
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/GestionClientes" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+                <BiGroup className="nav-icon" />
+                {!menuCollapsed && 'Clientes'}
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/GestionPaquetes" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+                <BiPackage className="nav-icon" />
+                {!menuCollapsed && 'Paquetes'}
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/rutas" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+                <BiDirections className="nav-icon" />
+                {!menuCollapsed && 'Rutas'}
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/vehiculos" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+                <BiCar className="nav-icon" />
+                {!menuCollapsed && 'Vehículos'}
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/historial" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+                <BiShoppingBag className="nav-icon" />
+                {!menuCollapsed && 'Historial'}
+              </NavLink>
+            </li>
+            <li className="nav-item mt-auto">
+              <NavLink to="/logout" className="nav-link">
+                <BiLogOut className="nav-icon" />
+                {!menuCollapsed && 'Logout'}
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className={`main-content ${menuCollapsed ? 'collapsed' : ''}`}>
         <Outlet />
       </div>
-      <Footer />
+      <Footer menuCollapsed={menuCollapsed} />
     </div>
   );
 };
