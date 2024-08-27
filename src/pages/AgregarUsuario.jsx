68467c2f3cd1c603ad5d66a66bf88a6d1762661a
William Rosa <williamrosaherrera@gmail.com>
2024-08-27 17:51:45 -0600

    adaptar alerta de agregar usuario a darkmode

--- src/pages/AgregarUsuario.jsx
+++ src/pages/AgregarUsuario.jsx
@@ -70,18 +70,32 @@ const AgregarUsuario = () => {
 
   const handleSuccessAlert = (id) => {
     confirmAlert({
-      title: 'Usuario agregado exitosamente!',
-      message: '¿Desea agregar la información del empleado ahora?',
-      buttons: [
-        {
-          label: 'Sí, crear ahora',
-          onClick: () => navigate(`/AgregarEmpleado/${id}`)
-        },
-        {
-          label: 'Agregar más tarde',
-          onClick: () => navigate('/GestionUsuarios')
-        }
-      ]
+      customUI: ({ onClose }) => {
+        return (
+          <div className="custom-dark-alert">
+            <h1>Usuario agregado exitosamente!</h1>
+            <p>¿Desea agregar la información del empleado ahora?</p>
+            <div className="alert-buttons">
+              <button
+                onClick={() => {
+                  navigate(`/AgregarEmpleado/${id}`);
+                  onClose();
+                }}
+              >
+                Sí, crear ahora
+              </button>
+              <button
+                onClick={() => {
+                  navigate('/GestionUsuarios');
+                  onClose();
+                }}
+              >
+                Agregar más tarde
+              </button>
+            </div>
+          </div>
+        );
+      }
     });
   };
 
