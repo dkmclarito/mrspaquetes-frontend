9ef155aadf6b6e9caf4396a911c65bacfdd9117e
Yancy Alfaro <alfaromargarita556@gmail.com>
2024-09-24 16:54:51 -0600

    orden entrega

--- src/pages/OrdenEntregada.jsx
+++ src/pages/OrdenEntregada.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import { Card, CardBody, Col, Row, Container, Form, FormGroup, Label, Input, Button } from "reactstrap";
 import Breadcrumbs from "../components/Vehiculos/Common/Breadcrumbs";
 import AuthService from "../services/authService";
@@ -6,21 +6,22 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import "../styles/Vehiculos.css";
+import jsQR from "jsqr";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 const OrdenEntregada = () => {
     const [idPaquete, setIdPaquete] = useState("");
     const [imagen, setImagen] = useState(null);
-    const [alertaError, setAlertaError] = useState(false);
-    const [errorMensaje, setErrorMensaje] = useState("");
-
+    const [escaneandoQR, setEscaneandoQR] = useState(false);
     const navigate = useNavigate();
     const token = AuthService.getCurrentUser();
+    const videoRefQR = useRef(null);
+    const canvasRefQR = useRef(null);
 
     const verificarEstadoUsuarioLogueado = useCallback(async () => {
         try {
-            const userId = localStorage.getItem("userId");
+            const userId =  localStorage.getItem("userId");
             if (userId && token) {
                 const response = await fetch(`${API_URL}/auth/show/${userId}`, {
                     headers: { Authorization: `Bearer ${token}` },
@@ -41,13 +42,9 @@ const OrdenEntregada = () => {
     }, [token]);
 
     useEffect(() => {
-        verificarEstadoUsuarioLogueado(); // Verifica el estado del usuario al cargar la página
-
-        const interval = setInterval(() => {
-            verificarEstadoUsuarioLogueado(); // Verifica el estado del usuario cada cierto tiempo
-        }, 30000); // Verifica cada 30 segundos
-
-        return () => clearInterval(interval); // Limpia el intervalo al desmontar el componente
+        verificarEstadoUsuarioLogueado();
+        const interval = setInterval(verificarEstadoUsuarioLogueado, 30000);
+        return () => clearInterval(interval);
     }, [verificarEstadoUsuarioLogueado]);
 
     const handleIdPaqueteChange = (e) => {
@@ -60,17 +57,78 @@ const OrdenEntregada = () => {
         }
     };
 
+    const iniciarEscaneoQR = useCallback(() => {
+        setEscaneandoQR(true);
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
+                .then(function(stream) {
+                    if (videoRefQR.current) {
+                        videoRefQR.current.srcObject = stream;
+                        videoRefQR.current.play();
+                    }
+                })
+                .catch(function(error) {
+                    console.error("No se pudo acceder a la cámara para QR", error);
+                    toast.error("No se pudo acceder a la cámara para escaneo de QR");
+                });
+        }
+    }, []);
+
+    const detenerEscaneoQR = useCallback(() => {
+        setEscaneandoQR(false);
+        if (videoRefQR.current && videoRefQR.current.srcObject) {
+            videoRefQR.current.srcObject.getTracks().forEach(track => track.stop());
+        }
+    }, []);
+
+    const escanearFrameQR = useCallback(() => {
+        if (videoRefQR.current && canvasRefQR.current && escaneandoQR) {
+            const canvas = canvasRefQR.current;
+            const video = videoRefQR.current;
+            canvas.height = video.videoHeight;
+            canvas.width = video.videoWidth;
+            const ctx = canvas.getContext('2d', { willReadFrequently: true });
+            if (ctx) {
+                ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+                const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                const code = jsQR(imageData.data, imageData.width, imageData.height, {
+                    inversionAttempts: "dontInvert",
+                });
+
+                if (code) {
+                    console.log("Código QR detectado:", code.data);
+                    setIdPaquete(code.data);
+                    detenerEscaneoQR();
+                    toast.success("Código QR escaneado con éxito");
+                }
+            }
+        }
+    }, [escaneandoQR, detenerEscaneoQR]);
+
+    useEffect(() => {
+        let intervalId;
+        if (escaneandoQR) {
+            intervalId = setInterval(escanearFrameQR, 100); // Escanea cada 100ms
+        }
+        return () => {
+            if (intervalId) {
+                clearInterval(intervalId);
+            }
+        };
+    }, [escaneandoQR, escanearFrameQR]);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         if (!idPaquete || !imagen) {
-            setErrorMensaje("Por favor, proporciona el codigo Qr del paquete y selecciona una imagen.");
-            toast.error("Por favor, completa todos los campos requeridos.", { position: "bottom-right",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true, });
+            toast.error("Por favor, proporciona el código QR del paquete y selecciona una imagen.", {
+                position: "bottom-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+            });
             return;
         }
 
@@ -86,13 +144,14 @@ const OrdenEntregada = () => {
                 },
             });
 
-            toast.success("Paquete validado exitosamente!", { position: "bottom-right",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true, });
-            //setTimeout(() => navigate('/GestionPaquetes'), 2000); // Redirigir después de 2 segundos
+            toast.success("Paquete validado exitosamente!", {
+                position: "bottom-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+            });
             resetForm();
         } catch (error) {
             handleError(error);
@@ -109,13 +168,12 @@ const OrdenEntregada = () => {
         if (error.response && error.response.data) {
             errorMessage = error.response.data.message || errorMessage;
         }
-        setErrorMensaje(errorMessage);
         toast.error(errorMessage, { position: "bottom-right", autoClose: 5000 });
     };
 
     return (
         <Container>
-            <Breadcrumbs title="Validación de Entrega del paquete" breadcrumbItem="Validar Entrega" />
+            <Breadcrumbs title="Validación de la Entrega del Paquete" breadcrumbItem="Validar Entrega" />
             <Row>
                 <Col lg="12">
                     <Card>
@@ -123,27 +181,61 @@ const OrdenEntregada = () => {
                             <Form onSubmit={handleSubmit}>
                                 <FormGroup>
                                     <Label for="idPaquete">Codigo Qr del Paquete</Label>
-                                    <Input
-                                        type="text"
-                                        id="idPaquete"
-                                        value={idPaquete}
-                                        onChange={handleIdPaqueteChange}
-                                        required
-                                    />
+                                    <div className="d-flex">
+                                        <Input
+                                            type="text"
+                                            id="idPaquete"
+                                            value={idPaquete}
+                                            onChange={handleIdPaqueteChange}
+                                            required
+                                            style={{ width: '60%'}}
+                                        />
+                                        <Button color="primary" onClick={iniciarEscaneoQR} disabled={escaneandoQR} style={{ marginLeft: '10px' }}>
+                                            Escanear QR
+                                        </Button>
+                                    </div>
                                 </FormGroup>
                                 <FormGroup>
                                     <Label for="imagen">Imagen de Entrega</Label>
                                     <Input
                                         type="file"
                                         id="imagen"
-                                        accept="image/*"  // Esto permite seleccionar solo imágenes y abre la cámara en dispositivos móviles
-                                        capture="environment"  // Esto intenta usar la cámara trasera del dispositivo
+                                        accept="image/*"
+                                        capture="environment"
                                         onChange={handleImagenChange}
                                         required
+                                        style={{ width: '70%'}}
                                     />
                                 </FormGroup>
                                 <Button type="submit" color="primary">Validar Entrega</Button>
                             </Form>
+                            {escaneandoQR && (
+                                <Row className="mt-3">
+                                    <Col md={12}>
+                                        <div style={{ position: 'relative', width: '100%', maxWidth: '400px', margin: '0 auto' }}>
+                                            <video 
+                                                ref={videoRefQR}
+                                                style={{ width: '100%', maxWidth: '400px', display: 'block' }}
+                                                playsInline
+                                            ></video>
+                                            <canvas 
+                                                ref={canvasRefQR}
+                                                style={{ 
+                                                    position: 'absolute', 
+                                                    top: 0, 
+                                                    left: 0, 
+                                                    width: '100%', 
+                                                    height: '100%',
+                                                    opacity: 0.5
+                                                }}
+                                            ></canvas>
+                                        </div>
+                                        <Button color="danger" onClick={detenerEscaneoQR} style={{ marginTop: '10px' }}>
+                                            Detener Escaneo QR
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            )}
                         </CardBody>
                     </Card>
                 </Col>
